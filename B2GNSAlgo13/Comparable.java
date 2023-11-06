82084545ede354aa748fbe132eb28e7030c531ab
EliasMeh <147180172+EliasMeh@users.noreply.github.com>
2023-11-06 04:55:40 +0100

    Add files via upload

---
+++ B2GNSAlgo13/Comparable.java
@@ -0,0 +1,6 @@
+package fr.gns.app;
+
+public interface Comparable {
+	public abstract int compareTo(Joueur autreJoueur);
+
+}
---
+++ B2GNSAlgo13/Joueur.java
@@ -0,0 +1,75 @@
+package fr.gns.app;
+
+public class Joueur implements Comparable {
+	private int numero ;
+	private String nom ;
+	private String pays ;
+	private int nbVictoires = 0 ;
+	
+	public Joueur(int numero, String nom, String pays, int nbVictoires) {
+		super();
+		this.numero = numero;
+		this.nom = nom;
+		this.pays = pays;
+		this.nbVictoires = nbVictoires;
+	}
+
+	public Joueur(int numero, String nom, String pays) {
+		super();
+		this.numero = numero;
+		this.nom = nom;
+		this.pays = pays;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPays() {
+		return pays;
+	}
+
+	public void setPays(String pays) {
+		this.pays = pays;
+	}
+
+	public int getNbVictoires() {
+		return nbVictoires;
+	}
+
+	public void setNbVictoires(int nbVictoires) {
+		this.nbVictoires = nbVictoires;
+	}
+
+	@Override
+	public String toString() {
+		return "Joueur [numero=" + numero + ", nom=" + nom + ", pays=" + pays + ", nbVictoires=" + nbVictoires + "]";
+	}
+
+	@Override
+	public int compareTo(Joueur autreJoueur) {
+	    if (this.getNbVictoires() > autreJoueur.getNbVictoires()) {
+	        return 1;
+	    } else if (this.getNbVictoires() == autreJoueur.getNbVictoires()) {
+	        return 0;
+	    } else {
+	        return -1;
+	    }
+	}
+	
+
+	
+
+}
